e41d726926c80345be42c1c6d39f27defa37afcd
andela-oofoamalu <jessicaprincess61@gmail.com>
2016-02-18 15:52:06 +0100

    Write tests for inverted-index

---
+++ jasmine/spec/inverted-index-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe("Inverted index test", function() {
+
+	describe("Read book data", function() {
+		it("Should read a JSON file and assert not empty", function() {
+			expect(true).toBe(true);
+		});
+	});
+
+	describe("Populate index", function() {
+		it("Should create an index once a JSON file is read", function() {
+
+		});
+
+		it("Should map the string keys to the correct objects in the JSON Array", function() {
+
+		});
+	});
+
+	describe("Search index", function() {
+		it("Should return an array of the indices of the correct objects that contain the words in the search query", function() {
+
+		});
+	});
+
+});
